a7157a7177ab27376fea71ef984bc4b79ca3ff39
mmuller99 <marco@remotemetering.net>
2016-04-15 13:26:22 +0200

    Add onError handler for tcp client to reject calling promise

---
+++ tests/tcp.connection.js
@@ -0,0 +1,48 @@
+var tcpConfig = require('./support/tcpConfig');
+var assert = require('assert');
+var eventstore = require('../index.js');
+var uuid = require('node-uuid');
+
+describe('TCP Client - Test Connection', function() {
+    it('Should connect and write event on correct connection properties', function() {
+        var client = eventstore.tcp(tcpConfig);
+        var testStream = 'TestStream-' + uuid.v4();
+        return client.writeEvent(testStream, 'TestEventType', {
+            something: '123'
+        });
+    });
+
+    it('Should not connect on incorrect hostname', function(done) {
+        var config = JSON.parse(JSON.stringify(tcpConfig));
+        config.hostname = 'MadeToFailHostName';
+
+        var client = eventstore.tcp(config);
+
+        var testStream = 'TestStream-' + uuid.v4();
+        return client.writeEvent(testStream, 'TestEventType', {
+            something: '123'
+        }).then(function() {
+            done('Should not have written event successfully');
+        }).catch(function(err) {
+            assert(err.message.indexOf('MadeToFailHostName') != -1, true)
+            done();
+        });
+    });
+
+    it('Should not connect on incorrect port', function(done) {
+        var config = JSON.parse(JSON.stringify(tcpConfig));
+        config.port = 9999;
+
+        var client = eventstore.tcp(config);
+
+        var testStream = 'TestStream-' + uuid.v4();
+        return client.writeEvent(testStream, 'TestEventType', {
+            something: '123'
+        }).then(function() {
+            done('Should not have written event successfully');
+        }).catch(function(err) {
+            assert(err.message.indexOf('9999') != -1, true)
+            done();
+        });
+    });
+});
