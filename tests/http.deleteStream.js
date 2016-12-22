f95a02c47642009dcda4827abfe7b2392b74c28e
mmuller99 <marco@remotemetering.net>
2016-12-22 12:03:55 -0200

    [Tests] add tests for hard deleting of streams

--- tests/http.deleteStream.js
+++ tests/http.deleteStream.js
@@ -23,6 +23,41 @@ describe('Http Client - Delete stream', function() {
         });
     });
 
+    it('Should return successful on writing to a stream that has been soft deleted', function() {
+        var client = eventstore.http(httpConfig);
+
+        var testStream = 'TestStream-' + uuid.v4();
+
+        return client.writeEvent(testStream, 'TestEventType', {
+            something: '123'
+        }).then(function() {
+            return client.deleteStream(testStream).then(function() {
+                return client.writeEvent(testStream, 'TestEventType', {
+                    something: '456'
+                });
+            }).catch(function(err) {
+                assert.fail(err.message);
+            });
+        });
+    });
+
+    it('Should return successful on stream delete hard delete', function() {
+        var client = eventstore.http(httpConfig);
+
+        var testStream = 'TestStream-' + uuid.v4();
+        return client.writeEvent(testStream, 'TestEventType', {
+            something: '123'
+        }).then(function() {
+            return client.deleteStream(testStream, true).then(function() {
+                return client.checkStreamExists(testStream).then(function(exists) {
+                    assert.equal(false, exists);
+                });
+            }).catch(function(err) {
+                assert.fail(err.message);
+            });
+        });
+    });
+
     it('Should fail when a stream does not exist', function() {
         var client = eventstore.http(httpConfig);
 
@@ -30,8 +65,28 @@ describe('Http Client - Delete stream', function() {
 
         return client.deleteStream(testStream).then(function() {
             assert.fail('Should have failed because stream does not exist');
-        }).catch(function(err){
+        }).catch(function(err) {
             assert(err);
+        });
+    });
+
+    it('Should return HTTP 410 when a writing to a stream that has been hard deleted', function() {
+        var client = eventstore.http(httpConfig);
+
+        var testStream = 'TestStream-' + uuid.v4();
+
+        return client.writeEvent(testStream, 'TestEventType', {
+            something: '123'
+        }).then(function() {
+            return client.deleteStream(testStream, true).then(function() {
+                return client.writeEvent(testStream, 'TestEventType', {
+                    something: '456'
+                }).then(function() {
+                    assert.fail('Should have failed because stream does not exist');
+                });
+            }).catch(function(err) {
+                assert.equal(410, err.statusCode);
+            });
         });
     });
 });
