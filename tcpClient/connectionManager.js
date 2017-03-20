21dd49d2bc9a023a72723e30a30d931c206d86e6
Rob <rjmarshall@remotemetering.net>
2017-03-20 10:33:40 +0200

    [fix] connection manager not create multiple connections

--- tcpClient/connectionManager.js
+++ tcpClient/connectionManager.js
@@ -3,17 +3,28 @@ var esClient = require('eventstore-node');
 var Promise = require('bluebird');
 var assert = require('assert');
 
+var waitDelay = 50;
 var connection;
+var isConnecting = false;
 
 var create = function(config) {
 	return Promise.resolve().then(function() {
 		assert(config.hostname, 'Hostname not provided');
 		assert(config.port, 'Port not provided');
-		if (connection) return connection;
+
+		if (connection && !isConnecting) return connection;
+		else if (isConnecting) {
+			return Promise.delay(waitDelay).then(function() {
+				return create(config);
+			});
+		}
+
+		isConnecting = true;
 		var esConnection = esClient.EventStoreConnection.create(config, `tcp://${config.hostname}:${config.port}`);
 		esConnection.on('closed', function(reason) {
 			debug('', 'Connection Closed:', reason);
 			if (esConnection === connection) {
+				isConnecting = false;
 				connection = undefined;
 			}
 		});
@@ -21,14 +32,20 @@ var create = function(config) {
 			console.error(err);
 			if (esConnection === connection) connection = undefined;
 			try {
+				isConnecting = false;
 				esConnection.close();
 			} catch (ex) {}
 		});
+		esConnection.on('connected', function() {
+			debug('', 'Connection Connected:');
+		});
 		return esConnection.connect().then(function() {
 			connection = esConnection;
+			isConnecting = false;
 			return esConnection;
 		}).catch(function(err) {
 			connection = undefined;
+			isConnecting = false;
 			throw err;
 		});
 	});
@@ -54,6 +71,9 @@ var closeAll = function() {
 				tempConnection.close();
 			} catch (ex) {}
 			reject(err);
+		});
+		connection.on('connected', function() {
+			debug('', 'Connection Connected:');
 		});
 		connection.close();
 	});
