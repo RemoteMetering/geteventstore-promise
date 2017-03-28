9e701445bcbe9671adc15f173dc48cd371644be9
mmuller99 <marco@remotemetering.net>
2017-03-28 18:57:41 -0200

    [Test] uncomment tests

--- tests/tcp.getAllStreamEvents.js
+++ tests/tcp.getAllStreamEvents.js
@@ -6,7 +6,7 @@ var assert = require('assert');
 var uuid = require('uuid');
 
 describe('TCP Client - Get All Stream Events', function() {
-    xit('Should write events and read back all stream events', function() {
+    it('Should write events and read back all stream events', function() {
         var client = eventstore.tcp(tcpConfig);
 
         var events = [];
@@ -26,7 +26,7 @@ describe('TCP Client - Get All Stream Events', function() {
         });
     });
 
-    xit('Should write events and read back all events from start event', function() {
+    it('Should write events and read back all events from start event', function() {
         var client = eventstore.tcp(tcpConfig);
 
         var events = [];
@@ -39,29 +39,6 @@ describe('TCP Client - Get All Stream Events', function() {
         var testStream = 'TestStream-' + uuid.v4();
         return client.writeEvents(testStream, events).then(function() {
             return client.getAllStreamEvents(testStream, 250, 500).then(function(events) {
-                assert.equal(events.length, 500);
-                assert.equal(events[0].data.id, 500);
-                assert.equal(events[499].data.id, 999);
-            });
-        });
-    });
-
-    it('TEST DEBUG INFLUENCE', function() {
-        this.timeout(100 * 1000);
-        var client = eventstore.tcp(tcpConfig);
-
-        var events = [];
-        for (var k = 0; k < 10000; k++) {
-            events.push(eventstore.eventFactory.NewEvent('TestEventType', {
-                id: k
-            }));
-        }
-
-        console.time('TIME !!!');
-        var testStream = 'TestStream-' + uuid.v4();
-        return client.writeEvents(testStream, events).then(function() {
-            return client.getAllStreamEvents(testStream, 250, 500).then(function(events) {
-                console.timeEnd('TIME !!!');
                 assert.equal(events.length, 500);
                 assert.equal(events[0].data.id, 500);
                 assert.equal(events[499].data.id, 999);
