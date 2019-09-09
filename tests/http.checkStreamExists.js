2f21857532db8930a56421aafdc2dffb6e0fb86c
Marco Muller <marco@remotemetering.net>
2019-09-09 09:28:01 +0200

    [Fix] flappy test

--- tests/http.checkStreamExists.js
+++ tests/http.checkStreamExists.js
@@ -23,9 +23,9 @@ describe('Http Client - Check Stream Exist', () => {
 	});
 
 	it('Should return rejected promise when the request error is anything other than a 404', callback => {
-		const config = JSON.parse(JSON.stringify(httpConfig));
-		config.port = 1;
-		const client = new EventStore.HTTPClient(config);
+		const clonedConfig = JSON.parse(JSON.stringify(httpConfig));
+		clonedConfig.port = 1;
+		const client = new EventStore.HTTPClient(clonedConfig);
 
 		client.checkStreamExists('Non_existentStream_wrong_port_config').then(() => {
 			callback('Should not have returned successful promise');
@@ -34,7 +34,7 @@ describe('Http Client - Check Stream Exist', () => {
 			assert(err.message.includes('ECONNREFUSED'), 'Connection refused error expected');
 			callback();
 		});
-	});
+	}).timeout(5000);
 
 	it('Should throw an exception when timeout is reached', callback => {
 		const clonedConfig = JSON.parse(JSON.stringify(httpConfig));
