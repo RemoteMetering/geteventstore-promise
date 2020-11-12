0258e5ff7a2e7406425b37423d18abfbaa1ab7b5
Jacek Zlowocki <jacek@billysbilling.com>
2020-11-12 17:41:58 +0100

    code style: spaces -> tabs

--- tests/tcp.connectToPersistentSubscription.js
+++ tests/tcp.connectToPersistentSubscription.js
@@ -10,24 +10,24 @@ import assert from 'assert';
 const eventFactory = new EventStore.EventFactory();
 
 describe('TCP Client - Connect to Persistent Subscription', () => {
-  const testSubscriptionName = 'TestStreamGroup1';
-  let testStream;
+	const testSubscriptionName = 'TestStreamGroup1';
+	let testStream;
 
-  // create persistent susbcription and populate with events
-  before(async function () {
-    const client = new EventStore.HTTPClient(httpConfig);
-    const initialEvents = [];
-    testStream = `TestStream-${generateEventId()}`;
+	// create persistent susbcription and populate with events
+	before(async function () {
+		const client = new EventStore.HTTPClient(httpConfig);
+		const initialEvents = [];
+		testStream = `TestStream-${generateEventId()}`;
 
-    for (let k = 0; k < 10; k++) {
+		for (let k = 0; k < 10; k++) {
 			initialEvents.push(eventFactory.newEvent('TestEventType', {
 				id: k
 			}));
 		}
 
 		await client.writeEvents(testStream, initialEvents);
-    await client.persistentSubscriptions.assert(testSubscriptionName, testStream);
-  })
+		await client.persistentSubscriptions.assert(testSubscriptionName, testStream);
+	})
 
 	it('Should get all events written to a subscription stream after subscription is started', function (done) {
 		this.timeout(15 * 1000);
@@ -43,12 +43,12 @@ describe('TCP Client - Connect to Persistent Subscription', () => {
 			if (!hasPassed) done('should not drop');
 		}
 
-    client.connectToPersistentSubscription(testStream, testSubscriptionName, onEventAppeared, onDropped).then(subscription => sleep(3000).then(() => {
-      assert.equal(10, processedEventCount, 'expect processed events to be 10');
-      assert(subscription, 'Subscription Expected');
-      hasPassed = true;
-      done();
-      client.close();
-    })).catch(done);
+		client.connectToPersistentSubscription(testStream, testSubscriptionName, onEventAppeared, onDropped).then(subscription => sleep(3000).then(() => {
+			assert.equal(10, processedEventCount, 'expect processed events to be 10');
+			assert(subscription, 'Subscription Expected');
+			hasPassed = true;
+			done();
+			client.close();
+		})).catch(done);
 	});
 });
