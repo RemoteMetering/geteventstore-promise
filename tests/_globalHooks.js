bcb674e754637c47b04569c62ec710321552fdc2
Marco Muller <marco@remotemetering.net>
2021-05-20 16:30:48 +0200

    [Tests] only tests = docker tests

--- tests/_globalHooks.js
+++ tests/_globalHooks.js
@@ -1,13 +1,15 @@
-import esConfig from './support/inMemEventStoreConfig';
+import httpConfig from './support/httpConfig';
+import tcpConfig from './support/tcpConfig';
 import sleep from './utilities/sleep';
 import { spawn } from 'child_process';
 import path from 'path';
 
 const clusterComposeFileLocation = path.join(__dirname, 'support', 'cluster', 'docker-compose.yml');
+const dockerContainerName = 'geteventstore-promise-test-eventstore';
 let eventstore;
 
 const removeContainer = async () => {
-	const removeProcess = spawn(`docker`, ['rm', '-f', esConfig.dockerContainerName], { detached: true });
+	const removeProcess = spawn(`docker`, ['rm', '-f', dockerContainerName], { detached: true });
 	removeProcess.unref();
 	await sleep(4000);
 };
@@ -16,23 +18,23 @@ const addContainer = async () => {
 	const dockerParameters = [
 		'run',
 		'--name',
-		esConfig.dockerContainerName,
+		dockerContainerName,
 		'--env=EVENTSTORE_INSECURE=true',
 		'--env=EVENTSTORE_MEM_DB=true',
 		'--env=EVENTSTORE_CLUSTER_SIZE=1',
 		'--env=EVENTSTORE_RUN_PROJECTIONS=All',
 		'--env=EVENTSTORE_START_STANDARD_PROJECTIONS=true',
-		`--env=EVENTSTORE_ADVERTISE_HTTP_PORT_TO_CLIENT_AS=${esConfig.options.extHttpPort}`,
-		`--env=EVENTSTORE_ADVERTISE_TCP_PORT_TO_CLIENT_AS=${esConfig.options.extTcpPort}`,
+		`--env=EVENTSTORE_ADVERTISE_HTTP_PORT_TO_CLIENT_AS=${httpConfig.port}`,
+		`--env=EVENTSTORE_ADVERTISE_TCP_PORT_TO_CLIENT_AS=${tcpConfig.port}`,
 		'--env=EVENTSTORE_DISCOVER_VIA_DNS=false',
 		'--env=EVENTSTORE_ENABLE_EXTERNAL_TCP=true',
 		'--env=EVENTSTORE_ENABLE_ATOM_PUB_OVER_HTTP=true',
 		'--env=EVENTSTORE_ADVERTISE_HOST_TO_CLIENT_AS=127.0.0.1',
 		'-d',
 		'-p',
-		`${esConfig.options.extHttpPort}:2113`,
+		`${httpConfig.port}:2113`,
 		'-p',
-		`${esConfig.options.extTcpPort}:1113`,
+		`${tcpConfig.port}:1113`,
 		'eventstore/eventstore:20.10.2-bionic'
 	];
 
@@ -70,41 +72,21 @@ const removeClusterContainers = async () => new Promise((resolve, reject) => {
 
 before(async function () {
 	this.timeout(60 * 1000);
+	if (eventstore) return;
 
-	if (eventstore === undefined) {
-		console.log('Starting in-mem ES...');
+	console.log('Starting in-mem ES...');
 
-		if (esConfig.testsUseDocker) {
-			await removeContainer();
-			await removeClusterContainers();
-			await addContainer();
-			await addClusterContainers();
-		} else {
-			const intTcpPort = `--int-tcp-port=${esConfig.options.intTcpPort}`;
-			const extTcpPort = `--ext-tcp-port=${esConfig.options.extTcpPort}`;
-			const intHttpPort = `--int-http-port=${esConfig.options.intHttpPort}`;
-			const extHttpPort = `--ext-http-port=${esConfig.options.extHttpPort}`;
+	await removeContainer();
+	await removeClusterContainers();
+	await addContainer();
+	await addClusterContainers();
 
-			eventstore = spawn(esConfig.executable, ['--mem-db=True', intTcpPort, extTcpPort, intHttpPort, extHttpPort, '--run-projections=ALL', '--start-standard-projections=True'], {
-				cwd: undefined,
-				stdio: ['ignore', 'ignore', process.stderr]
-			});
-
-			eventstore.on('exit', () => eventstore = undefined);
-		}
-
-		return sleep(10000);
-	}
-	return Promise.resolve();
+	return sleep(10000);
 });
 
 after(async function () {
 	this.timeout(60 * 1000);
 	console.log('Killing in-mem ES...');
-	if (eventstore) eventstore.kill();
-	eventstore = undefined;
-	if (esConfig.testsUseDocker) {
-		await removeContainer();
-		await removeClusterContainers();
-	}
+	await removeContainer();
+	await removeClusterContainers();
 });
--- tests/tcp.cluster.js
+++ tests/tcp.cluster.js
@@ -3,38 +3,37 @@ import './_globalHooks';
 import tcpConfigDNSDiscoveryCluster from './support/tcpConfigDNSDiscoveryCluster';
 import tcpConfigGossipCluster from './support/tcpConfigGossipCluster';
 import generateEventId from '../lib/utilities/generateEventId';
-import inMemConfig from './support/inMemEventStoreConfig';
 import EventStore from '../lib';
 import assert from 'assert';
 
 const eventFactory = new EventStore.EventFactory();
 
-if (inMemConfig.testsUseDocker) {
-	describe('TCP Client - Cluster', () => {
-		it('Write and read events using gossip seeds', async () => {
-			const client = new EventStore.TCPClient(tcpConfigGossipCluster);
+describe('TCP Client - Cluster', () => {
+	it('Write and read events using gossip seeds', async function () {
+		this.timeout(5 * 1000);
+		const client = new EventStore.TCPClient(tcpConfigGossipCluster);
 
-			const events = [eventFactory.newEvent('TestEventType', { something: '456' })];
-			const testStream = `TestStream-${generateEventId()}`;
-			await client.writeEvents(testStream, events);
+		const events = [eventFactory.newEvent('TestEventType', { something: '456' })];
+		const testStream = `TestStream-${generateEventId()}`;
+		await client.writeEvents(testStream, events);
 
-			const evs = await client.getEvents(testStream);
-			assert.equal(evs[0].data.something, '456');
+		const evs = await client.getEvents(testStream);
+		assert.equal(evs[0].data.something, '456');
 
-			await client.close();
-		});
-
-		it('Write and read events using DNS discovery', async () => {
-			const client = new EventStore.TCPClient(tcpConfigDNSDiscoveryCluster);
-
-			const events = [eventFactory.newEvent('TestEventType', { something: '456' })];
-			const testStream = `TestStream-${generateEventId()}`;
-			await client.writeEvents(testStream, events);
-
-			const evs = await client.getEvents(testStream);
-			assert.equal(evs[0].data.something, '456');
-
-			await client.close();
-		});
+		await client.close();
 	});
-}
+
+	it('Write and read events using DNS discovery', async function () {
+		this.timeout(5 * 1000);
+		const client = new EventStore.TCPClient(tcpConfigDNSDiscoveryCluster);
+
+		const events = [eventFactory.newEvent('TestEventType', { something: '456' })];
+		const testStream = `TestStream-${generateEventId()}`;
+		await client.writeEvents(testStream, events);
+
+		const evs = await client.getEvents(testStream);
+		assert.equal(evs[0].data.something, '456');
+
+		await client.close();
+	});
+});
